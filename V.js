2bff818131814a520101d43456d7a912d04a94a7
Levon <diplaySuper@gmail.com>
2014-02-19 22:14:28 +0400

    making BNF editor

--- V.js
+++ V.js
@@ -1,10 +1,14 @@
 function loadTask()
 {
 	getTask();
+
 	var taskDiv = document.getElementById("task");
 	taskDiv.innerHTML = "<span>" + currentTask.sentences[0] + "</span><br/>";
 	taskDiv.innerHTML += "<span>" + currentTask.sentences[1] + "</span><br/>";
 	taskDiv.innerHTML += "<span>" + currentTask.sentences[2] + "</span><br/>";
+
+	var list = ["lol", "lolka", "lololol"];
+	initBNF(list, document.getElementById("BNF"));
 }
 
 window.onload = loadTask;
