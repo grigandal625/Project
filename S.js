0b2ba3ecbdf6e7e042906ac54202b8256d62b37c
Bod V. Den <alzater@mail.ru>
2014-02-20 18:47:05 +0400

    Add reset.css to your projects!

--- S.js
+++ S.js
@@ -1,12 +1,5 @@
-function loadTask()
-{
+sentenceS = {};
+$(document).ready(function(){
 	getTask();
-
-	var taskDiv = document.getElementById("sentence1DivS");
-	taskDiv.innerHTML =  currentTask.sentences[0];
-	
-
-
-}
-
-window.onload = loadTask;
+	$("#sentence1DivS").html(currentTask.sentences[0]);	
+});
