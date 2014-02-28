d9b727c628cdf0298f7760029e286afe738a42a9
diplay <diplaySuper@gmail.com>
2014-02-28 14:06:00 +0400

    begin working on server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UIR::Application.config.secret_key_base = '01e1cb0a14c527b6eff7114b61370f2c0ee429ce07b1d042ea2214a6bf2ccc4c65abe9d23179eeda41aaa2a973d7ecd1bc900275511f5401569579d687e4d3e6'
