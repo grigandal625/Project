896227ac998fda1db9138395ae14d2989420783d
Elshiko <Minak.S@mail.ru>
2014-03-10 13:47:05 +0400

    cleaned

--- app/models/g_answer.rb
+++ app/models/g_answer.rb
@@ -1,3 +1,9 @@
 class GAnswer < ActiveRecord::Base
   belongs_to :task
+  
+  def check_answer(groups_to_check, bnf_to_check)
+     obj_groups = JSON.parse(groups)
+     obj_bnf = JSON.parse(bnf)
+  end
+  
 end
