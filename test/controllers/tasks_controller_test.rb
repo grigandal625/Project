edf9bb7bc640626df30f77a4f25617c99f54d65d
Levon <diplaySuper@gmail.com>
2014-03-17 12:52:27 +0400

    fixed tasts

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  test "should get get_v" do
-    get :get_v
+  test "should get get_task" do
+    get :get_task
     assert_response :success
   end
 
