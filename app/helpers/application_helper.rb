a1e88f99bb39a71009303f6281e8d79768d02504
diplay <diplaySuper@gmail.com>
2014-03-20 20:09:58 +0400

    bug fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,4 @@
 #coding: utf-8
 module ApplicationHelper
-  def bnf_elements_lists
-    return {["<падеж>", "padezh"] => Padezh_list,
-            ["<род>", "gender"] => Gender_list,
-            ["<число>", "number"] => Number_list,
-            ["<одушевленность>", "odush"] => Odush_list,
-            ["<вид>", "vid"] => Vid_list,
-            ["<время>", "tense"] => Tense_list,
-            ["<имя семантической валентности>", "sem_val"] => Sem_val_list,
-            ["<предлог>", "pred"] => Pred_list,
-            ["<семантический признак>", "sem_prizn"] => Sem_prizn_list,
-            ["<кодификатор части речи>", "codificator"] => Codificator_list}
-  end
 
 end
