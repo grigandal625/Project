d00f156b6baa05c4685669f7a922c369bc4a7b6e
alzater <alzater@mail.ru>
2014-03-23 13:41:59 +0400

    fix bugs

--- app/models/s_answer.rb
+++ app/models/s_answer.rb
@@ -9,31 +9,29 @@ class SAnswer < ActiveRecord::Base
     standart_answer = JSON.parse(answer)
     for i in 0..2
       if c_answer[i] == nil
-        mark = 0
-        break
+        return 0
       end
       if c_answer[i].length < 2
-        mark = 0
-        break
+        return 0
       end
-      if c_answer[i][0][:num] != standart_answer[i][0][:num]
+      if c_answer[i][0]["num"] != standart_answer[i][0]["num"]
         mark -= 5
       end
-      if c_answer[i][0][:word] != standart_answer[i][0][:word]
+      if c_answer[i][0]["word"] != standart_answer[i][0]["word"]
         mark -= 5
       end
-      if c_answer[i][0][:param] != standart_answer[i][0][:param]
+      if c_answer[i][0]["param"] != standart_answer[i][0]["param"]
         mark -= 5
       end
 
       len = c_answer[i].length
       parents = []
       for j in 1...len
-        parents[standart_answer[i][j][:level]] = standart_answer[i][j][:num]
-        if standart_answer[i][j][:level] == 1
+        parents[standart_answer[i][j]["level"].to_i] = standart_answer[i][j]["num"]
+        if standart_answer[i][j]["level"] == 1
           k = 1
           while k < c_answer[i].length
-            if c_answer[i][k][:num] == standart_answer[i][j][:num]
+            if c_answer[i][k]["num"] == standart_answer[i][j]["num"]
               break
             end
             k += 1
@@ -41,21 +39,21 @@ class SAnswer < ActiveRecord::Base
           if k == c_answer[k].length
             mark -= 5
           else
-            if c_answer[i][k][:word] != standart_answer[i][j][:word]
+            if c_answer[i][k]["word"] != standart_answer[i][j]["word"]
               mark -= 3
             end
-            if c_answer[i][k][:level] != standart_answer[i][j][:level]
+            if c_answer[i][k]["level"] != standart_answer[i][j]["level"]
               mark -= 5
             end
-            if c_answer[i][k][:param] != standart_answer[i][j][:param]
+            if c_answer[i][k]["param"] != standart_answer[i][j]["param"]
               mark -= 3
             end
-            c_answer[i][k][:seen] = 1
+            c_answer[i][k]["seen"] = 1
           end  
         else
           k = 1
           while k < c_answer[i].length
-            if c_answer[i][k][:num] == standart_answer[i][j][:num]
+            if c_answer[i][k]["num"] == standart_answer[i][j]["num"]
               break
             end
             k += 1
@@ -63,26 +61,26 @@ class SAnswer < ActiveRecord::Base
           if k == c_answer[i].length
             mark -= 2
           else
-            if c_answer[i][k][:word] != standart_answer[i][j][:word]
+            if c_answer[i][k]["word"] != standart_answer[i][j]["word"]
               mark -= 2
             end
-            if c_answer[i][k][:level] != standart_answer[i][j][:level]
+            if c_answer[i][k]["level"] != standart_answer[i][j]["level"]
               mark -= 2
             end
-            if c_answer[i][k][:level] == 1
+            if c_answer[i][k]["level"] == 1
               mark -= 3
             end
-            if c_answer[i][k][:param] != standart_answer[i][j][:param]
+            if c_answer[i][k]["param"] != standart_answer[i][j]["param"]
               mark -= 2
             end
             t = k
-            while c_answer[i][k][:level] >= c_answer[i][t][:level]             
+            while c_answer[i][k]["level"] >= c_answer[i][t]["level"]             
               t -= 1
             end 
-            if parents[c_answer[i][t][:level]] != c_answer[i][t][:num] 
+            if parents[c_answer[i][t]["level"].to_i] != c_answer[i][t]["num"] 
               mark -= 2
             end
-            c_answer[i][k][:seen] = 1
+            c_answer[i][k]["seen"] = 1
           end 
 
         end    
@@ -90,8 +88,8 @@ class SAnswer < ActiveRecord::Base
 
       len = c_answer[i].length
       for j in 1...len
-        if c_answer[i][j][:seen] == nil
-          if c_answer[i][j][:level] == 1
+        if c_answer[i][j]["seen"] == nil
+          if c_answer[i][j]["level"] == 1
             mark -= 5
           else
             mark -= 3
