357dc8522873ead9a7572e9d782071904b114a4a
diplay <diplaySuper@gmail.com>
2014-03-28 12:17:29 +0400

    workaround in G task editor

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,7 +35,7 @@ class TasksController < AdminToolsController
   def update
     task = Task.find(params[:id])
     task.v_answer.set_rules(params[:bnf])
-    task.g_answer.answer = params[:Ganswer]
+    task.g_answer.answer = params[:Ganswer] unless params[:ignore_g]
     sentences = []
     params[:sentences].split("\r\n").each{|sen| sentences << sen unless sen == ""}
     task.update_attributes(sentence1: sentences[0], sentence2: sentences[1],
