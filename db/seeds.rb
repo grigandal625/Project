47765ff0af18cc622c73df9d289181794fd480c5
Elshiko <Minak.S@mail.ru>
2014-04-18 11:55:56 +0400

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-task = Task.create(sentence1: 'Зимой 2014 года в красивый курортный город Сочи впервые смогут приехать спортсмены из раличных стран мира на Олимпийские игры.',
-  sentence2: 'Сколько спортсменов и тренеров из разных стран мира хотят приехать в город Сочи с 1 февраля 2014 года по 28 февраля 2014 года на Олимпийские игры?',
-  sentence3: '1-го мая 2010 года необходимо срочно отправить в командировку в город Сочи на олимпийские объекты членов олимпийского комитета РФ.')
-task.create_v_answer
-task.create_g_answer(answer: '{"bnf":[{"left":"&lt;G&gt;","rules":[["&lt;Предложение типа вопрос&gt;"],["&lt;Предложение типа сообщение&gt;"],["&lt;Предложение типа команда&gt;"]]},{"left":null,"rules":[]}], "groups":{"0":{"status":0,"type":"group","data":"Зимой 2014 года","sentence":"0","groupId":5,"groupName":"ИГ 1"},"1":{"status":0,"type":"group","data":"в красивый курортный город Сочи","sentence":"0","groupId":6,"groupName":"ИГ 2"},"2":{"status":0,"type":"group","data":"впервые","sentence":"0","groupId":3,"groupName":"Н"},"3":{"status":0,"type":"group","data":"смогут приехать","sentence":"0","groupId":1,"groupName":"П"},"4":{"status":0,"type":"group","data":"спортсмены из раличных стран мира","sentence":"0","groupId":7,"groupName":"ИГ 3"},"5":{"status":0,"type":"group","data":"на Олимпийские игры.","sentence":"0","groupId":8,"groupName":"ИГ 4"},"6":{"status":0,"type":"group","data":"Сколько","sentence":"1","groupId":4,"groupName":"ВС"},"7":{"status":0,"type":"group","data":"спортсменов","sentence":"1","groupId":9,"groupName":"ИГ 5"},"8":{"status":0,"type":"group","data":"и","sentence":"1","groupId":2,"groupName":"С"},"9":{"status":0,"type":"group","data":"тренеров из разных стран мира","sentence":"1","groupId":7,"groupName":"ИГ 3"},"10":{"status":0,"type":"group","data":"хотят приехать","sentence":"1","groupId":1,"groupName":"П"},"11":{"status":0,"type":"group","data":"в город Сочи","sentence":"1","groupId":10,"groupName":"ИГ 6"},"12":{"status":0,"type":"group","data":"с 1 февраля 2014 года","sentence":"1","groupId":11,"groupName":"ИГ 7"},"13":{"status":0,"type":"group","data":"по 28 февраля 2014 года","sentence":"1","groupId":11,"groupName":"ИГ 7"},"14":{"status":0,"type":"group","data":"на Олимпийские игры?","sentence":"1","groupId":8,"groupName":"ИГ 4"},"15":{"status":0,"type":"group","data":"1-го мая 2010 года","sentence":"2","groupId":12,"groupName":"ИГ 8"},"16":{"status":0,"type":"group","data":"необходимо срочно отправить в командировку","sentence":"2","groupId":1,"groupName":"П"},"17":{"status":0,"type":"group","data":"в город Сочи на олимпийские объекты","sentence":"2","groupId":13,"groupName":"ИГ 9"},"18":{"status":0,"type":"group","data":"членов олимпийского комитета РФ.","sentence":"2","groupId":13,"groupName":"ИГ 10"}}}')
-task.v_answer.create_bnf(bnf_json: {"<число>" => "",
-"<род>" => "",
-"<одушевленность>" => "",
-"<вид>" => "",
-"<время>" => "",
-"<предлог>" => "",
-"<семантический признак>" => "",
-"<кодификатор части речи>" => "",
-"<падеж>" => "",
-"<имя семантической валентности>" => "",
-"<лицо>" => "",
- }.to_json)
-task.create_s_answer(answer: '[[{"num":"6","word":"","param":"00","level":"0"},{"num":"5","word":"","param":"0","level":"1"}],[{"num":"2","word":"","param":"00","level":"0"},{"num":"4","word":"","param":"0","level":"1"}],[{"num":"5","word":"","param":"00","level":"0"},{"num":"6","word":"","param":"0","level":"1"}]]')
-
 task = Task.create(sentence1: 'По итогам научной конференции МИФИ-2008 срочно произвести награждение   дипломами и премиями МИФИ  лучших студентов в  актовом зале МИФИ с 10 до 12.',
   sentence2: 'Какими дипломами будут награждены лучшие студенты МИФИ в актовом зале в апреле 2008 года?',
   sentence3: 'На научную конференцию в город Н в сентябре 2008 года могут поехать студенты старших курсов МИФИ  с докладами.')
@@ -44,11 +25,12 @@ task.v_answer.create_bnf(bnf_json: {"<число>" => "Единственное|
  }.to_json)
 task.create_s_answer(answer: '[[{"num":"7","word":"наградить","param":"31","level":"0"},{"num":"16","word":"студент","param":"4","level":"1"},{"num":"15","word":"лучший","param":"8","level":"2"},{"num":"1","word":"итог","param":"17","level":"1"},{"num":"3","word":"конференция","param":"8","level":"2"},{"num":"4","word":"МИФИ-2008","param":"8","level":"3"},{"num":"2","word":"научная","param":"8","level":"3"},{"num":"10","word":"диплом","param":"13","level":"1"},{"num":"12","word":"премия","param":"13","level":"1"},{"num":"13","word":"МИФИ","param":"8","level":"2"},{"num":"21","word":"МИФИ","param":"11","level":"1"},{"num":"20","word":"зал","param":"14","level":"2"},{"num":"19","word":"актовый","param":"8","level":"3"}],[{"num":"1","word":"","param":"00","level":"0"},{"num":"5","word":"","param":"0","level":"1"},{"num":"6","word":"","param":"0","level":"1"},{"num":"4","word":"","param":"0","level":"1"},{"num":"9","word":"","param":"0","level":"1"},{"num":"8","word":"","param":"0","level":"1"},{"num":"12","word":"","param":"0","level":"1"},{"num":"13","word":"","param":"0","level":"1"},{"num":"11","word":"","param":"0","level":"1"},{"num":"3","word":"","param":"0","level":"1"}],[{"num":"11","word":"поехать","param":"31","level":"0"},{"num":"12","word":"студент","param":"1","level":"1"},{"num":"15","word":"мифи","param":"8","level":"2"},{"num":"14","word":"курс","param":"8","level":"2"},{"num":"13","word":"старший","param":"8","level":"3"},{"num":"18","word":"доклад","param":"17","level":"1"},{"num":"4","word":"город","param":"6","level":"1"},{"num":"5","word":"Н","param":"8","level":"2"},{"num":"2","word":"конференция","param":"7","level":"1"},{"num":"1","word":"научная","param":"8","level":"2"},{"num":"8","word":"2008","param":"19","level":"1"},{"num":"9","word":"год","param":"8","level":"2"},{"num":"7","word":"сентябрь","param":"14","level":"3"}]]')
 
-k6_224 = Group.create(number: 'K6-224')
-st = k6_224.students.create(fio: "Test Student")
+test_group = Group.create(number: 'Test group')
+st = test_group.students.create(fio: "Test Student")
 testuser = st.create_user(login: 'student0', pass: Digest::MD5.hexdigest('lolka'),
                       role: 'student')
-admin = User.create(login: 'admin', pass: Digest::MD5.hexdigest('admin'),
+
+admin = User.create(login: 'admin', pass: Digest::MD5.hexdigest('lgkofpes'),
                    role: 'admin')
 iAppleJack = User.create(login: 'iAppleJack', pass: Digest::MD5.hexdigest('Pooh0007'),
                    role: 'admin')
