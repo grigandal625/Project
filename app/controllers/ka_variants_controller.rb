33c3644b0665f0d6311ca77a749630f4e373cea5
duran-duran <bender1511@gmail.com>
2015-04-16 21:32:27 +0300

    Detailed results output and few fixes

--- app/controllers/ka_variants_controller.rb
+++ app/controllers/ka_variants_controller.rb
@@ -21,38 +21,35 @@ class KaVariantsController < ApplicationController
     question_marks = {}
     questions = variant.ka_question.each
     questions.each do |q|
-      question_marks[q.id] = [0.0, 0.0, 0.0]
-      # question_marks[q.id].right_count = q.answer.where(correct: 1).count
-      # question_marks[q.id].wrong_count = q.answer.where(correct: 0).count
-      question_marks[q.id][0] = q.ka_answer.where(correct: 1).count
-      question_marks[q.id][1] = q.ka_answer.where(correct: 0).count
+      question_marks[q.id] = {right_ans_count: 0.0, wrong_ans_count: 0.0, score: 0.0}
+      question_marks[q.id][:right_ans_count] = q.ka_answer.where(correct: 1).count
+      question_marks[q.id][:wrong_ans_count] = q.ka_answer.where(correct: 0).count
     end
 
     answers.each do |a, _buf|
       ans = KaAnswer.find(a)
       if ans.correct != 0
-        question_marks[ans.ka_question_id][2] += 1.0 / question_marks[ans.ka_question_id][0]
+        question_marks[ans.ka_question_id][:score] += 1.0 / question_marks[ans.ka_question_id][:right_ans_count]
       else
-        question_marks[ans.ka_question_id][2] -= 1.0 / question_marks[ans.ka_question_id][1]
+        question_marks[ans.ka_question_id][:score] -= 1.0 / question_marks[ans.ka_question_id][:wrong_ans_count]
       end
     end
 
-    sum_score = 0
-    sum_diff = 0
+    sum_score = 0.0
+    sum_diff = 0.0
 
     topic_marks = {}
 
     questions.each do |q|
-      topic_marks[q.ka_topic_id] = [0.0, 0.0, 0.0]
+      topic_marks[q.ka_topic_id] = {sum_score: 0.0, sum_diff: 0.0, score: 0.0}
     end
 
     questions.each do |q|
-      sum_score += q.difficulty * question_marks[q.id][2]
+      sum_score += q.difficulty * question_marks[q.id][:score]
       sum_diff += q.difficulty
-      # topic_marks[q.topic_id].sum_score += q.difficulty * question_marks[q.id].score
-      # topic_marks[q.topic_id].sum_diff += q.difficulty
-      topic_marks[q.ka_topic_id][0] += q.difficulty * question_marks[q.id][2]
-      topic_marks[q.ka_topic_id][1] += q.difficulty
+
+      topic_marks[q.ka_topic_id][:sum_score] += q.difficulty * question_marks[q.id][:score]
+      topic_marks[q.ka_topic_id][:sum_diff] += q.difficulty
      end
 
     @assessment = sum_score / sum_diff
@@ -80,31 +77,28 @@ class KaVariantsController < ApplicationController
       competence_marks = {}
 
       topic_marks.each do |id, t|
-        # topic_marks.each do |id, t|
-        # t.score = t.sum_score / t.sum_diff
-        t[2] = t[0] / t[1]
-        t[2] = 0 if t[2] < 0
-        ProblemArea.create(ka_result_id: result.id, ka_topic_id: id, mark: t[2])
+        t[:score] = t[:sum_score] / t[:sum_diff]
+        t[:score] = 0 if t[:score] < 0
+        ProblemArea.create(ka_result_id: result.id, ka_topic_id: id, mark: t[:score])
 
         top = KaTopic.find(id)
         top.topic_competences.each do |c|
           if not competence_marks.has_key?(c.competence_id)
-            competence_marks[c.competence_id] = [0.0, 0.0, 0.0]
+            competence_marks[c.competence_id] = {sum_score: 0.0, sum_weight: 0.0, score: 0.0}
           end
-          # competence_marks[c.competence_id].sum_weight += c.weight
-          # competence_marks[c.competence_id].sum_score += t.score * c.weight
-          competence_marks[c.competence_id][0] += c.weight
-          competence_marks[c.competence_id][1] += t[2] * c.weight
+          competence_marks[c.competence_id][:sum_score] += t[:score] * c.weight
+          competence_marks[c.competence_id][:sum_weight] += c.weight
         end
       end
 
       competence_marks.each do |id, c|
-        c[2] = c[1] / c[0]
-        CompetenceCoverage.create(ka_result_id: result.id, competence_id: id, mark: c[2])
+        c[:score] = c[:sum_score] / c[:sum_weight]
+        CompetenceCoverage.create(ka_result_id: result.id, competence_id: id, mark: c[:score])
       end
     end
 
-    @result = result
-    @question_marks = question_marks
+    #Для полного вывода
+    #@result = result
+    #@question_marks = question_marks
   end
 end
