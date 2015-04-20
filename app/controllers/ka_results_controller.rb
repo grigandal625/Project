9b7c348b71abde3e02a909028e14359735b3ed97
Avitella <urtashev@gmail.com>
2015-04-20 15:20:35 +0000

    Tracer added

--- app/controllers/ka_results_controller.rb
+++ app/controllers/ka_results_controller.rb
@@ -14,5 +14,19 @@ class KaResultsController < ApplicationController
     result_id = params[:result_id]
     @result = KaResult.find(result_id)
     @student = @result.user.student
+
+    answers = []
+    @result.ka_answer_logs.each do |al|
+      answers.push(al.ka_answer_id)
+    end
+
+    @detail_result = KaDetailResult.new(@result.ka_variant, answers)
+
+    @detail_result.detail_questions.each do |q_id, q|
+      puts q.question.text + " " + q.assessment.to_s
+      q.detail_answers.each do |a_id, a|
+        puts "  " + a.answer.text + " " + a.real_correct.to_s + " " + a.student_correct.to_s
+      end
+    end
   end
 end
--- app/models/ka_result.rb
+++ app/models/ka_result.rb
@@ -1,3 +1,105 @@
+class DetailAnswer
+  def initialize(answer)
+    @student_correct = 0
+    @answer = answer
+    @real_correct = answer.correct
+  end
+
+  def answer
+    @answer
+  end
+
+  def student_correct
+    @student_correct
+  end
+
+  def student_correct=(x)
+    @student_correct = x
+  end
+
+  def real_correct
+    @real_correct
+  end
+end
+
+class DetailQuestionResult
+  def initialize(question)
+    @assessment = 0
+    @question = question
+    @detail_answers = {}
+  end
+
+  def detail_answers
+    @detail_answers
+  end
+
+  def assessment=(x)
+    @assessment = x
+  end
+
+  def assessment
+    @assessment
+  end
+
+  def question
+    @question
+  end
+end
+
+class KaDetailResult
+  def initialize(variant, answers)
+    @assessment = 0
+    @detail_questions = {}
+
+    question_marks = {}
+    questions = variant.ka_question.each
+
+    questions.each do |q|
+      question_marks[q.id] = {right_ans_count: 0.0, wrong_ans_count: 0.0, score: 0.0}
+      question_marks[q.id][:right_ans_count] = q.ka_answer.where(correct: 1).count
+      question_marks[q.id][:wrong_ans_count] = q.ka_answer.where(correct: 0).count
+
+      @detail_questions[q.id] = DetailQuestionResult.new(q)
+      q.ka_answer.each do |ans|
+        @detail_questions[q.id].detail_answers[ans.id] = DetailAnswer.new(ans)
+      end
+    end
+
+    answers.each do |a, _buf|
+      ans = KaAnswer.find(a)
+      if ans.correct != 0
+        question_marks[ans.ka_question_id][:score] += 1.0 / question_marks[ans.ka_question_id][:right_ans_count]
+      else
+        question_marks[ans.ka_question_id][:score] -= 1.0 / question_marks[ans.ka_question_id][:wrong_ans_count]
+      end
+      @detail_questions[ans.ka_question_id].detail_answers[ans.id].student_correct = 1
+    end
+
+    sum_score = 0
+    sum_diff = 0
+
+    questions.each do |q|
+      sum_score += q.difficulty * question_marks[q.id][:score]
+      sum_diff += q.difficulty
+      @detail_questions[q.id].assessment = q.difficulty * question_marks[q.id][:score]
+     end
+
+    @assessment = sum_score / sum_diff
+    if @assessment < 0
+      @assessment = 0
+    end
+    @assessment = @assessment.to_i
+  end
+
+  def detail_questions
+    @detail_questions
+  end
+
+  def assessment
+    @assessment
+  end
+end
+
 class KaResult < ActiveRecord::Base
   belongs_to :user
   belongs_to :ka_variant
