e4f555581ea4df4a145393b3a8052c593b704916
Andrew <grigandal625@gmail.com>
2024-12-21 17:29:54 +0300

    upd style tmp

--- app/controllers/ka_variants_controller.rb
+++ app/controllers/ka_variants_controller.rb
@@ -7,10 +7,10 @@ class KaVariantsController < ApplicationController
   def check
     variant = KaVariant.find(params[:id])
     @error = 0
-    # if KaResult.where(user_id: @user.id, ka_test_id: variant.ka_test.id).count > 0 and @user.role != "admin"
-    #   @error = 1
-    #   return
-    # end
+    if KaResult.where(user_id: @user.id, ka_test_id: variant.ka_test.id).count > 0 and @user.role != "admin"
+      @error = 1
+      return
+    end
 
     @assessment = 0
     answers = {}
